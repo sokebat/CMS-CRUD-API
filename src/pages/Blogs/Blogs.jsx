dcfb75fe7746ef736da7965973384adc77736eed
Ashish <sokebat01@gmail.com>
2023-11-26 17:06:14 +0545

    feat: fetch mock api

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get(
+      "https://6562fbc6ee04015769a6ada1.mockapi.io/blog"
+    );
+    console.log(response);
+    if (response.status == "200") {
+      setBlogs(response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className=" grid grid-cols-4 gap-3   mx-auto">
+      {blogs.map((blog, index) => {
+        const { title, description, createdAt, avatar } = blog;
+        return (
+          <>
+            <div
+              key={index}
+              class="max-w-md  m-5 bg-white  overflow-hidden rounded-2xl  shadow-xl"
+            >
+              <img
+                class="w-full h-64 object-cover object-center"
+                src={avatar}
+                alt={title}
+              />
 
-export default Blogs
+              <div class="p-6">
+                <h2 class="font-bold text-xl mb-2">{title}</h2>
 
+                <p class="text-gray-600 text-sm">
+                  Published on <span class="text-gray-800">{createdAt}</span>
+                </p>
 
- 
-  git init
-  git add .
-  git commit -m "project setup"
-  git branch -M main
-  git remote add origin https://github.com/sokebat/Content-Management-System-with-REACT.git
-  git push -u origin main
+                <p class="text-gray-700 mt-4">{description}</p>
+
+                <a href="#" class="text-blue-500 mt-4 inline-block">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Blogs;
